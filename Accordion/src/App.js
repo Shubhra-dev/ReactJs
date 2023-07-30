2af2897dbb17c88f1e506fc369bc6f12f46d2f19
Shubhradev Chakrabarty <89873720+Shubhra-dev@users.noreply.github.com>
2023-07-30 11:58:03 +0600

    State lifted up one level for keeping the other accordion closed while opening one

--- Accordion/src/App.js
+++ Accordion/src/App.js
@@ -22,31 +22,43 @@ const faqs = [
 export default function App() {
   return (
     <div>
-      <Accordion />
+      <Accordion data={faqs} />
     </div>
   );
 }
 
-function Accordion() {
+function Accordion({ data }) {
+  const [curOpen, setCurOpen] = useState(null);
   return (
     <div className="accordion">
-      {faqs.map((el, i) => (
-        <AccordionItem title={el.title} text={el.text} num={i} key={el.title} />
+      {data.map((el, i) => (
+        <AccordionItem
+          curOpen={curOpen}
+          title={el.title}
+          text={el.text}
+          num={i}
+          key={el.title}
+          handleOpen={setCurOpen}
+        />
       ))}
     </div>
   );
 }
-function AccordionItem({ num, title, text }) {
-  const [isOpen, setIsOpen] = useState(false);
-  function handleToogle() {
-    setIsOpen((isOpen) => !isOpen);
+
+function AccordionItem({ curOpen, num, title, text, handleOpen }) {
+  const isOpen = num === curOpen;
+
+  function handleToggle() {
+    handleOpen(isOpen ? null : num);
   }
+
   return (
-    <div className={`item ${isOpen ? "open" : ""}`} onClick={handleToogle}>
-      <p className="number">{num}</p>
+    <div className={`item ${isOpen ? "open" : ""}`} onClick={handleToggle}>
+      <p className="number">{num < 9 ? `0${num + 1}` : num + 1}</p>
       <p className="title">{title}</p>
       <p className="icon">{isOpen ? "-" : "+"}</p>
-      {isOpen ? <div className="content-box">{text}</div> : ""}
+
+      {isOpen && <div className="content-box">{text}</div>}
     </div>
   );
 }
