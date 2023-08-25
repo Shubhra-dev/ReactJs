a921c8dbb7e1e96c4f2b003a398f4e83fe93749a
Shubhra-dev <shuuvro.dev@gmail.com>
2023-08-25 13:45:53 +0600

    API data data fetching clwan up used properly

--- movie_popcorn/src/App.js
+++ movie_popcorn/src/App.js
@@ -15,12 +15,14 @@ export default function App() {
 
   useEffect(
     function () {
+      const controller = new AbortController();
       async function fetchMovies() {
         setError("");
         setIsLoading(true);
         try {
           const res = await fetch(
-            `http://www.omdbapi.com/?apikey=${KEY}&s=${query}`
+            `http://www.omdbapi.com/?apikey=${KEY}&s=${query}`,
+            { signal: controller.signal }
           );
           if (!res.ok) {
             throw new Error("Something Went Wrong!! 🔄");
@@ -31,8 +33,11 @@ export default function App() {
             throw new Error("Movie Not Found!! ❌");
           }
           setMovies(data.Search);
+          setError("");
         } catch (err) {
-          setError(err.message);
+          if (err.name !== "AbortError") {
+            setError(err.message);
+          }
         } finally {
           setIsLoading(false);
         }
@@ -43,6 +48,10 @@ export default function App() {
         return;
       }
       fetchMovies();
+
+      return function () {
+        controller.abort();
+      };
     },
     [query]
   );
