25b37622977b826737b50e3add6b654afd04f6b1
Shubhra-dev <shuuvro.dev@gmail.com>
2023-09-11 23:03:46 +0600

    Fake API created from questions.json file for fetching questions

--- react_quiz/src/App.js
+++ react_quiz/src/App.js
@@ -1,8 +1,26 @@
-import DateCounter from "./DateCounter";
+import { useEffect, useReducer } from "react";
+import Header from "./Header";
+import Main from "./Main";
+const initialState = {
+  questions:[];
+  
+};
+function reducer(state, action) {}
 export default function App() {
+  const [state, dispatch] = useReducer(reducer, initialState);
+  useEffect(function () {
+    fetch("http://localhost:8000/questions")
+      .then((res) => res.json())
+      .then((data) => console.log(data))
+      .catch((err) => console.log(err));
+  }, []);
   return (
-    <div>
-      <DateCounter />
+    <div className="app">
+      <Header />
+      <Main>
+        <p>1/15</p>
+        <p>Question??</p>
+      </Main>
     </div>
   );
 }
