52c6bd7790f695c1c7bd5fbf39048ff1f16ce817
Shubhra-dev <shuuvro.dev@gmail.com>
2023-09-14 00:02:19 +0600

    useReducer used for state handling in different status

--- react_quiz/src/App.js
+++ react_quiz/src/App.js
@@ -1,25 +1,36 @@
 import { useEffect, useReducer } from "react";
 import Header from "./Header";
 import Main from "./Main";
+import Loader from "./Loader";
+import Error from "./Error";
 const initialState = {
-  questions:[];
-  
+  questions: [],
+  status: "loading",
 };
-function reducer(state, action) {}
+function reducer(state, action) {
+  switch (action.type) {
+    case "dataReceived":
+      return { ...state, questions: action.payload, status: "ready" };
+    case "dataFailed":
+      return { ...state, status: "error" };
+    default:
+      throw new Error("Not Known");
+  }
+}
 export default function App() {
   const [state, dispatch] = useReducer(reducer, initialState);
   useEffect(function () {
-    fetch("http://localhost:8000/questions")
+    fetch("http://localhost:9000/questions")
       .then((res) => res.json())
-      .then((data) => console.log(data))
-      .catch((err) => console.log(err));
+      .then((data) => dispatch({ type: "dataReceived", payload: data }))
+      .catch((err) => dispatch({ type: "dataFailed" }));
   }, []);
   return (
     <div className="app">
       <Header />
       <Main>
-        <p>1/15</p>
-        <p>Question??</p>
+        {state.status === "loading" && <Loader />}
+        {state.status === "error" && <Error />}
       </Main>
     </div>
   );
